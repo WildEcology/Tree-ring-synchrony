808229b6a18ae9b85f3989dd5466119fe6acefca
Kaitlyn McKnight <kmcknig2@uwyo.edu>
2021-11-29 12:25:37 -0700

    calculated quantiles for both annual precipitation and monthly precipitation - added new 'percentile' column to new precipitation dataframes for new measures of drought

--- Scripts/wavelet_script.R
+++ Scripts/wavelet_script.R
@@ -68,3 +68,59 @@ test4 <- left_join(test3, rwi_70s, by = c("year", "plot"))%>%
 
 # removing any trees with missing data along the timeseries
 rwi_70s_filtered<- test4[rowSums(is.na(test4))==0,]
+
+
+# determine precipitation quantiles for measures of drought (annual)
+
+ppt_filter_an <- anprismplot %>%
+  filter(variable == "ppt")
+
+quant_det_an <- ppt_filter %>%
+  summarize(quant = quantile(value))
+
+print(quant_det_an)
+
+# low = 12.36750
+# low-med = 49.41833
+# med = 73.73000
+# high-med = 105.42667
+# high = 250.75000
+
+# create new column for precipitation percentiles
+quant_ppt_an <-  ppt_filter_an %>%
+  mutate(percentile = case_when(value < 12.36750 ~ "20",
+                                value > 12.36750 & value <= 49.41833 ~ "40",
+                                value > 49.41834 & value <= 73.7300 ~ "60",
+                                value > 72.7300 & value <= 105.42667 ~ "80",
+                                value > 105.42667 & value <= 250.7500 ~ "100")) %>%
+  separate(plot_id, c("site", "plotnum", "assignment"), sep = "_") %>%
+  unite("plot_id_needle", site:plotnum, sep = "_", remove = TRUE)%>%
+  rename(ppt_raw = value)
+
+# determine precipitation quantiles for measures of drought (monthly)
+
+ppt_filter_mo <- moprismplot %>%
+  filter(variable == "ppt")
+
+quant_det_mo <- ppt_filter_mo %>%
+  summarize(quant = quantile(value))
+
+print(quant_det_mo)
+
+# low = 2.436667
+# low-med = 17.280000
+# med = 25.863333
+# high-med = 35.795000
+# high = 96.220000
+
+# create new column for precipitation percentiles
+
+quant_ppt_mo <-  ppt_filter_mo %>%
+  mutate(percentile = case_when(value < 2.436667 ~ "20",
+                                value > 2.436667 & value <=  17.280000 ~ "40",
+                                value >  17.280000 & value <= 25.863333 ~ "60",
+                                value > 25.863333 & value <= 35.795000 ~ "80",
+                                value > 35.795000 & value <= 96.220000 ~ "100")) %>%
+  separate(plot_id, c("site", "plotnum", "assignment"), sep = "_") %>%
+  unite("plot_id_needle", site:plotnum, sep = "_", remove = TRUE)%>%
+  rename(ppt_raw = value)
